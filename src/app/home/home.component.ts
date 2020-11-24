d4afdac968f02fd77a869b407824c19f5e235cc9
Amanda Diana <amandadianacmd@gmail.com>
2020-11-24 11:07:26 +0100

    first init

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { AuthService } from "../auth.service";
+
+@Component({
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
+})
+export class HomeComponent implements OnInit {
+  isLogedIn: string = "Loged Out";
+
+  constructor(private router: Router, private authService: AuthService) {}
+
+  ngOnInit() {}
+
+  onLoadServer(id) {
+    this.router.navigate(["/servers", id, "edit"], {
+      queryParams: { allowEdit: "1" },
+      fragment: "loading",
+    });
+  }
+
+  onLogin() {
+    this.authService.login();
+    this.isLogedIn = "Loged in";
+  }
+
+  onLogout() {
+    this.authService.logout();
+    this.isLogedIn = "Loged Out";
+  }
+}
